a319b02a6df28c1c844a3b8e8c31756c64a5a208
David Casey <caseydj@dukes.jmu.edu>
2016-12-30 23:09:38 -0500

    something

---
+++ desktop/src/com/david/casey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.david.casey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.david.casey.game2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new game2(), config);
+	}
+}
